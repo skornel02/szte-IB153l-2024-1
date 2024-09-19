7738f59d9734870d0da1723e4a28c441501a23b7
Kornél <stefankornel02@gmail.com>
2024-09-19 19:34:38 +0200

    fix services

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -3,6 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
