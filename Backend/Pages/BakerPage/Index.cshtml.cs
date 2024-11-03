d74189ba86de6b2ff2aa43c45059b3071e923c86
Kinguska <vass.kinga.fanni@stud.u-szeged.hu>
2024-11-03 18:33:58 +0100

    Fix some things in BakerPage

--- Backend/Pages/BakerPage/Index.cshtml.cs
+++ Backend/Pages/BakerPage/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Backend.Attributes;
 using Backend.Entities;
 using Backend.Persistence;
 using Microsoft.AspNetCore.Mvc;
@@ -6,12 +7,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Pages.BakerPage
 {
-    public class IndexModel : PageModel
+    [RoleAuthorize([Enums.UserRole.Admin, Enums.UserRole.Baker])]
+    public class IndexModel : BasePageModel
     {
         private readonly BellaDbContext _context;
 
         public List<ProductEntity> Products { get; set; } = null!;
 
+        public ProductIngredientsEntity ProductIngredientsEntity { get; set; } = default!;
+
+
+        [BindProperty]
         public int NumberToMake { get; set; }
 
         public IndexModel(BellaDbContext context)
@@ -25,7 +31,32 @@ namespace Backend.Pages.BakerPage
                 .Include(_ => _.Ingredients)
                     .ThenInclude(_ => _.Ingredient)
                 .ToListAsync();
-           
+
+            
+        }
+
+        public async Task<IActionResult> OnPostAsync(Guid? id)
+        {
+
+            if (id == null)
+            {
+                return RedirectToPage("./Index", new { ErrorMessage = "Product not found!" });
+            }
+
+            var productEntity = await _context.Products
+                .Include(_ => _.Ingredients)
+                .ThenInclude(_ => _.Ingredient)
+                .FirstOrDefaultAsync(_ => _.Id == id);
+            if (productEntity != null)
+            {
+                productEntity.Stock += NumberToMake;
+                foreach (var item in productEntity.Ingredients)
+                {
+                    item.Ingredient.Stock -= item.Quantity * NumberToMake;
+                }
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage("./Index", new { SuccessMessage = "Product(s) created successfully!" });
         }
     }
 }
