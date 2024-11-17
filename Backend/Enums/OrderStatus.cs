3af5aa8293ffe8a906feb1089eff9ee084dfb311
Kinguska <vass.kinga.fanni@stud.u-szeged.hu>
2024-11-17 16:02:09 +0100

    Fix Digital Sales page order status

--- Backend/Enums/OrderStatus.cs
+++ Backend/Enums/OrderStatus.cs
@@ -1,4 +1,6 @@
-﻿namespace Backend.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Enums;
 
 // TODO: flow chart
 public enum OrderStatus
@@ -7,29 +9,36 @@ public enum OrderStatus
     /// Order is currently in the cart. 
     /// These items are not reserved.
     /// </summary>
+    [Display(Name = "Order in cart")]
     OrderInCart,
     /// <summary>
     /// The order has been placed. Needs approval.
     /// </summary>
+    [Display(Name = "Order placed")]
     OrderPlaced,
     /// <summary>
     /// The order has been rejected.
     /// </summary>
+    [Display(Name = "Order rejected")]
     OrderRejected,
     /// <summary>
     /// The order has been approved. Waiting for pickup
     /// </summary>
+    [Display(Name = "Order approved")]
     OrderApproved,
     /// <summary>
     /// The order has been cancelled before shipping.
     /// </summary>
+    [Display(Name = "Order cancelled")]
     OrderCancelled,
     /// <summary>
     /// The order has been shipped.
     /// </summary>
+    [Display(Name = "Order shipped")]
     OrderShipped,
     /// <summary>
     /// The order has been succefully delivered.
     /// </summary>
+    [Display(Name = "Order delivered")]
     OrderFinished,
 }
